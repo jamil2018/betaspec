3ce033190d2e7da03a52a728b0b42c183edbe7cb
Hasnat Jamil <hasnatjamil2018@gmail.com>
2022-08-14 02:59:05 +0600

    changeset: preparing boilerplate

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,21 @@
+import { Typography } from "@mui/material";
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Betaspec</title>
+        <meta
+          name="description"
+          content="Next generation test tracking and test case recording tool"
+        />
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+      <Typography variant="h1">Hello world</Typography>
+    </div>
+  );
+};
+
+export default Home;
---
+++ client/src/pages/login/index.tsx
@@ -0,0 +1,8 @@
+import { Typography } from "@mui/material";
+import { NextPage } from "next";
+
+const Login: NextPage = () => {
+  return <Typography variant="h1">Login page</Typography>;
+};
+
+export default Login;
